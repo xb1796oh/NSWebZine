d198e4bd648b9b18ab9e43afa41a3f8e4839ad14
xb1796oh <vhqltkfkd24@gmail.com>
2022-05-17 13:48:18 +0900

    스케쥴러 bean 설정

--- webzine/src/main/java/com/nds/webzine/WebzineApplication.java
+++ webzine/src/main/java/com/nds/webzine/WebzineApplication.java
@@ -2,12 +2,24 @@ package com.nds.webzine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebzineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebzineApplication.class, args);
 	}
-
+	
+	@Bean
+	public TaskScheduler scheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(4);
+		return scheduler;
+	}
+	
 }
